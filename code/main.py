760032b40182693566d7eb1e7b1c5d97ee133667
Andres Vargas <devandresvaras@gmail.com>
2022-10-22 17:06:57 -0500

    modify laser speed

--- code/main.py
+++ code/main.py
@@ -17,7 +17,7 @@ class Game:
 if __name__ == "__main__":
     pygame.init()
     screen_width = 600
-    screen_height = 600
+    screen_height = 600 
     screen = pygame.display.set_mode((screen_width,screen_height))
     clock = pygame.time.Clock()
     game = Game()
