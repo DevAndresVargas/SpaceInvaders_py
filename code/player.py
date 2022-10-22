8cf84f4310165608b8b3ea594c1bd23443550f24
andres vargas <devandresvargas@gmail.com>
2022-10-22 11:22:14 -0500

    constaint player

--- code/player.py
+++ code/player.py
@@ -1,7 +1,27 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos):
+    def __init__(self,pos,max_width,speed):
         super().__init__()
         self.image = pygame.image.load('../graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = speed
+        self.max_width = max_width
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+    def constraint(self):
+        if self.rect.left <=0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_width:
+            self.rect.right = self.max_width
+
+    def update(self):
+        self.get_input()
+        self.constraint()
