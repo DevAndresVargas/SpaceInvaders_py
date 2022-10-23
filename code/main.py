e8c58e3e775b2f830646bcd865fcec3b048bde97
andres vargas <devandresvargas@gmail.com>
2022-10-23 01:04:44 -0500

    add aliens sprites

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from player import Player
 import obstacle
+from alien import Alien
+
 
 class Game:
     def __init__(self):
@@ -15,7 +17,13 @@ class Game:
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) \
                                      for num in range(self.obstacle_amount)]
-        self.create_multiple_obstacles(self.obstacle_x_positions,x_init = screen_width / 15,y_init = 480)
+
+        # Alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows = 6, cols = 8)
+
+        self.create_multiple_obstacles(self.obstacle_x_positions,x_init =\
+                                       screen_width / 15,y_init = 480)
 
     def create_obstacle(self,x_init,y_init, offset_x):
         for row_index,row in enumerate(self.shape):
@@ -30,6 +38,26 @@ class Game:
         for offset_x in offset:
             self.create_obstacle(x_init,y_init,offset_x)
 
+    def alien_setup(self,rows,cols,x_distance = 60,y_distance = 40,\
+                    x_offset = 70,y_offset = 100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+
+                match row_index:
+                    case 0:
+                        color = 'yellow'
+                    case 1:
+                        color = 'green'
+                    case 2:
+                        color = 'green'
+                    case _:
+                        color = 'red'
+
+                alien_sprite = Alien(color,x,y)
+                self.aliens.add(alien_sprite)
+
     def run(self):
         self.player.update()
 
@@ -37,6 +65,8 @@ class Game:
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+
+        self.aliens.draw(screen)
         # update all sprite groups
         # draw all sprite groups
 
